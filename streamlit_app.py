4a17808620768ebd92676401dd1a115f846948a9
mintBomber <131649216+mintBomber@users.noreply.github.com>
2024-09-29 21:17:42 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
+from PIL import Image
+import numpy as np
+import cv2
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Image Converter")
+  
+uploaded_file = st.file_uploader('Upload your image file.')
+  
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    image = np.array(image)
+
+    st.subheader('Uploaded image')
+    st.image(image)
+
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    st.subheader('Converted image')
+    st.image(gray_image)
